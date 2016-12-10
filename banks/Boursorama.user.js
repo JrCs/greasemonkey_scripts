d3cfed4a17afb53e6563a7a63323356794a9c2b9
JrCs <90z7oey02@sneakemail.com>
2016-12-10 13:15:37 +0100

    Enhance OCR

--- banks/Boursorama.user.js
+++ banks/Boursorama.user.js
@@ -6,7 +6,7 @@
 // @description   Remove virtual keyboard and add a classic input text field for the password on Boursorama website.
 // @include       https://clients.boursorama.com/connexion*
 // @include       http://localhost:5789/boursorama/Boursorama%20Banque.html*
-// @version       1.0
+// @version       1.1
 // @updateURL     https://github.com/JrCs/greasemonkey_scripts/raw/master/banks/Boursorama.user.js
 // @require       http://code.jquery.com/jquery-1.11.1.min.js
 // @require       https://raw.githubusercontent.com/SheetJS/js-crc32/master/crc32.js
@@ -23,24 +23,24 @@ try {
 
     // Can't use GM_info with NinjaKit for safari
     var scriptName = 'Boursorama - No Virtual Keyboard'
-    var version = '1.0';
+    var version = '1.1';
 
     var debug = false;
     //var debug = true;
 
     var crcToNumber = new Object();
 
-    crcToNumber["2A190808"] = -1
-    crcToNumber["14C79344"] = 0
-    crcToNumber["321A2A99"] = 1
-    crcToNumber["70E5E999"] = 2
-    crcToNumber["B6447353"] = 3
-    crcToNumber["C89A4D3B"] = 4
-    crcToNumber["5C8290F1"] = 5
-    crcToNumber["F164402B"] = 6
-    crcToNumber["371846FF"] = 7
-    crcToNumber["F219A449"] = 8
-    crcToNumber["DA6970E6"] = 9
+    crcToNumber["4F17CCB5"] = -1
+    crcToNumber["4E2080F5"] = 0
+    crcToNumber["B13EE005"] = 1
+    crcToNumber["5BF41AB9"] = 2
+    crcToNumber["1A26C428"] = 3
+    crcToNumber["A30DE187"] = 4
+    crcToNumber["A71C7A43"] = 5
+    crcToNumber["E45A9C31"] = 6
+    crcToNumber["46AA49E1"] = 7
+    crcToNumber["81F01724"] = 8
+    crcToNumber["39B9DA69"] = 9
 
     var number2GridPosition;
     var padDecoded = false;
@@ -64,8 +64,7 @@ try {
     function convertColor(image_data, threshold) {
         var pix = image_data.data;
         for (var i = 0, n = pix.length; i < n; i += 4) {
-            var luma  = Math.floor(pix[i] * .299 + pix[i + 1] * 0.587 + pix[i + 2] * 0.114);
-            var color = luma >= threshold ? 255 : 0;
+            var color = pix[i] > threshold && pix[i+1] > threshold && pix[i+2] > threshold && pix[i+3] > threshold ? 255 : 0;
             pix[i]     = color; // red
             pix[i + 1] = color; // green
             pix[i + 2] = color; // blue
@@ -91,6 +90,8 @@ try {
     function decodeGrid($grid) {
         var nbCols = 4;
         var nbRows = 3;
+        var cutX   = 16;
+        var cutY   = 6;
 
         var canvas, ctx, imageData;
 
@@ -116,16 +117,16 @@ try {
                 var ctx = canvas.getContext("2d");
                 ctx.drawImage(img, 0, 0);
 
-                var imageData = ctx.getImageData(0, 0, img.width, img.height);
-                var pixels    = convertColor(imageData, 128); // return a buffer of pixels encoded in 32bit
+                var imageData = ctx.getImageData(cutX, cutY, img.width-(cutX*2), (img.height/2)-cutY-2);
+                var pixels    = convertColor(imageData, 250); // return a buffer of pixels encoded in 32bit
                 var imageCRC  = dec32ToHex(CRC32.buf(pixels));
                 var number    = crcToNumber[imageCRC];
 
                 if (debug) {
-                    ctx.putImageData(imageData, 0, 0) // Fill canvas with the thresholded image
+                    ctx.putImageData(imageData, cutX, cutY) // Fill canvas with the thresholded image
                     var $numberElement =
                             $("<span>")
-                            .css({ 'style': 'border-bottom: 1px solid red', 'font-family': '"Lucida Console", Monaco, monospace' })
+                            .css({ 'color':'yellow', 'background-color':'black', 'font-family': '"Lucida Console", Monaco, monospace' })
                             .text(" row=" + y + "; col=" + x +
                                   "; gridPosition=" + gridPosition + "; crc=" + imageCRC + " = " + number)
                     $body.append(canvas, $numberElement, '<br/>')
@@ -200,12 +201,12 @@ try {
         // Add another Cancel button (style take from original)
         var $cancelButton=
                 $('<a id="gm_cancel_button" style="display: inline;" href="javascript://" class="form-row__fake-password__cancel"><span class="icon-close2"></span></a>')
-          .on('click', function() {
-              $divControlPasswordInput.val("").trigger("change") // Clear input password when Cancel button is click
-          })
+                .on('click', function() {
+                    $divControlPasswordInput.val("").trigger("change") // Clear input password when Cancel button is click
+                })
 
-          var $fakepasswordContainer = $('div[data-name="fakePassword"] .form-row__widget')
-          $fakepasswordContainer.append($divControlPasswordInput, $cancelButton)
+        var $fakepasswordContainer = $('div[data-name="fakePassword"] .form-row__widget')
+        $fakepasswordContainer.append($divControlPasswordInput, $cancelButton)
 
         $('#form_fakePassword', $fakepasswordContainer).toggle(debug) // Display or not the original password input
     }
